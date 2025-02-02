c0da120c3bb5d1f6756d7b2838e19ed7e398feb3
Mia Pedersen <miap@student.uia.no>
2025-02-02 16:30:33 +0100

    Added tests for task a

--- a.mjs
+++ a.mjs
@@ -20,7 +20,8 @@ import test from "./test.mjs";
 //#region function -----------------------------------------------------------------
 // Write your function her.
 
-
+function multiply(numbers) {
+}
 
 //#endregion
 
@@ -30,6 +31,26 @@ import test from "./test.mjs";
 
 //#region Tests --------------------------------------------------------------------
 // Write your tests her.
+
+const tests = test("Sum function");
+
+// Valid inputs
+tests.isEqual(multiply(1, 2), 3, "Product of 1 and 2 should be 2");
+tests.isEqual(multiply(-5, 5), -25, "Product of -5 and 5 should be -25");
+tests.isEqual(multiply(2.5, 3.5), 8.75, "Product of 2.5 and 3.5 should be 8.75");
+tests.isEqual(multiply(5, 10), 50, "Product of 2, 3 and 4 should be 24");
+
+
+// Invalid inputs
+tests.isNotANumber(multiply("1", 2), 'Product of "1" and 2 should return NaN');
+tests.isNotANumber(multiply(1, null), "Product of 1 and null should return NaN");
+tests.isNotANumber(multiply(undefined, 3), "Product of undefined and 3 should return NaN");
+
+
+// Edge cases
+tests.isEqual(multiply(0, 0), 0, "Product of 0 and 0 should be 0");
+tests.isEqual(multiply(Infinity, 1), Infinity, "Product of Infinity and 1 should be Infinity");
+tests.isEqual(multiply(-Infinity, 1), -Infinity, "Product of -Infinity and 1 should be -Infinity");
 
 
 
